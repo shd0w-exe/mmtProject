ce8c75ebb51fc673ef19e1aab80c3b6896958f56
KAttal007 <kaushalattal03@gmail.com>
2022-10-04 11:52:07 +0530

    made changes to hotel.java

--- MMTProject/src/main/java/com/mmt/hotels/model/Hotel.java
+++ MMTProject/src/main/java/com/mmt/hotels/model/Hotel.java
@@ -1,7 +1,83 @@
 package com.mmt.hotels.model;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+import com.mmt.address.Address;
+import com.mmt.sequencegenerator.StringPrefixedSequenceIdGenerator;
+
+@Entity
+@Table(name = "hoteldetails")
 public class Hotel {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "address_seq")
+	@GenericGenerator(name = "address_seq", strategy = "com.mmt.sequencegenerator.StringPrefixedSequenceIdGenerator", parameters = {
+			@Parameter(name = StringPrefixedSequenceIdGenerator.INCREMENT_PARAM, value = "1"),
+			@Parameter(name = StringPrefixedSequenceIdGenerator.VALUE_PREFIX_PARAMETER, value = "Address_"),
+			@Parameter(name = StringPrefixedSequenceIdGenerator.NUMBER_FORMAT_PARAMETER, value = "%05d") })
 	private String hotelId;
 	private String hotelName;
-	
+	private String hotelBrand;
+
+	private List<Room> typesOfRooms;
+	private float priceAccToRoom;
+
+	private Address hotelAdd;
+
+	public String getHotelId() {
+		return hotelId;
+	}
+
+	public void setHotelId(String hotelId) {
+		this.hotelId = hotelId;
+	}
+
+	public String getHotelName() {
+		return hotelName;
+	}
+
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+
+	public String getHotelBrand() {
+		return hotelBrand;
+	}
+
+	public void setHotelBrand(String hotelBrand) {
+		this.hotelBrand = hotelBrand;
+	}
+
+	public List<Room> getTypesOfRooms() {
+		return typesOfRooms;
+	}
+
+	public void setTypesOfRooms(List<Room> typesOfRooms) {
+		this.typesOfRooms = typesOfRooms;
+	}
+
+	public float getPriceAccToRoom() {
+		return priceAccToRoom;
+	}
+
+	public void setPriceAccToRoom(float priceAccToRoom) {
+		this.priceAccToRoom = priceAccToRoom;
+	}
+
+	public Address getHotelAdd() {
+		return hotelAdd;
+	}
+
+	public void setHotelAdd(Address hotelAdd) {
+		this.hotelAdd = hotelAdd;
+	}
+
 }
