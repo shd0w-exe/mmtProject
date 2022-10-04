7da11f725a186ab2bed384d9b803f5cbc43bf2c4
Shadow <Shadow@Shadow>
2022-10-04 09:47:10 +0530

    bookedHotel

--- MMTProject/src/main/java/com/mmt/bookedHotel/BookedHotel.java
+++ MMTProject/src/main/java/com/mmt/bookedHotel/BookedHotel.java
@@ -1,14 +1,74 @@
 package com.mmt.bookedHotel;
 
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
 import com.mmt.hotels.model.Hotel;
+import com.mmt.sequencegenerator.StringPrefixedSequenceIdGenerator;
 import com.mmt.user.model.User;
 
 public class BookedHotel {
+	@Id
+	@GeneratedValue(strategy =GenerationType.SEQUENCE,generator = "bookedHotel_seq")
+	@GenericGenerator(name="bookedHotel_seq",
+	            strategy = "com.mmt.sequencegenerator.StringPrefixedSequenceIdGenerator",
+	            parameters = {
+	                    @Parameter(name=StringPrefixedSequenceIdGenerator.INCREMENT_PARAM,value="1"),
+	                    @Parameter(name=StringPrefixedSequenceIdGenerator.VALUE_PREFIX_PARAMETER,value="bookedHotel_"),
+	                    @Parameter(name=StringPrefixedSequenceIdGenerator.NUMBER_FORMAT_PARAMETER,value="%05d")
+	            }
+	            )
 	private String bookedHotelId;
 	private int noOfRooms;
 	private boolean isAc;
 	private int price;
+	@OneToOne
 	private Hotel hotel;
+	@ManyToOne
 	private User user;
+	public String getBookedHotelId() {
+		return bookedHotelId;
+	}
+	public void setBookedHotelId(String bookedHotelId) {
+		this.bookedHotelId = bookedHotelId;
+	}
+	public int getNoOfRooms() {
+		return noOfRooms;
+	}
+	public void setNoOfRooms(int noOfRooms) {
+		this.noOfRooms = noOfRooms;
+	}
+	public boolean isAc() {
+		return isAc;
+	}
+	public void setAc(boolean isAc) {
+		this.isAc = isAc;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public Hotel getHotel() {
+		return hotel;
+	}
+	public void setHotel(Hotel hotel) {
+		this.hotel = hotel;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
 }
