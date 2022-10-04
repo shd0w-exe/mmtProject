df6cec2c21db0478462d01b686c4d5bd0b23b11d
Shadow <Shadow@Shadow>
2022-10-04 09:27:55 +0530

    BookedFlight

--- MMTProject/src/main/java/com/mmt/bookedFlight/BookedFlight.java
+++ MMTProject/src/main/java/com/mmt/bookedFlight/BookedFlight.java
@@ -1,17 +1,70 @@
 package com.mmt.bookedFlight;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 import com.mmt.flights.model.Flight;
+import com.mmt.sequencegenerator.StringPrefixedSequenceIdGenerator;
 import com.mmt.user.model.User;
 
 @Entity
 public class BookedFlight {
 	@Id
+	@GeneratedValue(strategy =GenerationType.SEQUENCE,generator = "bookedFlight_seq")
+	@GenericGenerator(name="bookedFlight_seq",
+	            strategy = "com.mmt.sequencegenerator.StringPrefixedSequenceIdGenerator",
+	            parameters = {
+	                    @Parameter(name=StringPrefixedSequenceIdGenerator.INCREMENT_PARAM,value="1"),
+	                    @Parameter(name=StringPrefixedSequenceIdGenerator.VALUE_PREFIX_PARAMETER,value="BookedFlight_"),
+	                    @Parameter(name=StringPrefixedSequenceIdGenerator.NUMBER_FORMAT_PARAMETER,value="%05d")
+	            }
+	            )
 	private String bookedFlightId;
 	private int noOfSeats;
 	private int price;
+	@OneToOne
 	private Flight flight;
+	@ManyToOne
 	private User user;
+	public String getBookedFlightId() {
+		return bookedFlightId;
+	}
+	public void setBookedFlightId(String bookedFlightId) {
+		this.bookedFlightId = bookedFlightId;
+	}
+	public int getNoOfSeats() {
+		return noOfSeats;
+	}
+	public void setNoOfSeats(int noOfSeats) {
+		this.noOfSeats = noOfSeats;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public Flight getFlight() {
+		return flight;
+	}
+	public void setFlight(Flight flight) {
+		this.flight = flight;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+	
+	
 }
