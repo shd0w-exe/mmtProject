995a31b42a1ef7d1314f98ec6f9e058ec8991d72
Shadow <Shadow@Shadow>
2022-10-06 03:02:47 +0530

    ad

---
+++ MMTProject/src/main/java/com/mmt/admin/controllers/AdminController.java
@@ -0,0 +1,12 @@
+package com.mmt.admin.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+	@RequestMapping("adminLoginNav")
+	public String adminLogin() {
+		return "adminLoginPage";
+	}
+}
